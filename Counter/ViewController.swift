d98c9b709ec11767c7366aa01e286a435fca4424
Mahsa <haghnevismahsa@gmail.com>
2024-07-29 09:50:18 +0330

    Initial commit
    Create a new project for the counter app

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Mahsa on 7/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
