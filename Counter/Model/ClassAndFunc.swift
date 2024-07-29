3dad8a9dc3ded363cdec2720d03b0ffdde1ec2b1
Mahsa <haghnevismahsa@gmail.com>
2024-07-29 11:07:12 +0330

    feat: add lock functionality to Logic class
     Updated `CountState` array to include step values 1, 2, and 5.
    - Added `IsLocked` property to manage locking state.
    - Modified `Increase` and `Decrease` methods to check `IsLocked` before updating `Countnumber`.
    - Implemented `Lock` and `UnLock` methods to control the `IsLocked` state.

--- Counter/Model/ClassAndFunc.swift
+++ Counter/Model/ClassAndFunc.swift
@@ -10,25 +10,37 @@ import Foundation
 
 class Logic {
     private (set) var Countnumber : Int = 0
-    private var CountState : [Int] = [ 1 , 2, 5 , 10 ] //available step values
+    private var CountState : [Int] = [ 1, 2, 5 ] //available step values
     private (set) var Step  = 1       //the step value
     private var index = 0           //keeping track of step
-
+    private (set) var IsLocked : Bool = false //
     func ChangeSpeed(){ //in case use hit the step button
         
-        index=(index + 1)%4   //to handle return to head after hitting the tail
+        index=(index + 1)%3  //to handle return to head after hitting the tail
         Step = CountState[index]
     }
     
     func Increase(){
-        Countnumber += Step
+        if IsLocked != true {
+            Countnumber += Step
+        }
     }
     func Decreasee(){
-        Countnumber -= Step
+        if IsLocked != true {
+            Countnumber -= Step
+        }
     }
     
     func Reset(){
         Countnumber = 0
+    }
+    
+    func Lock(){
+        IsLocked=true
+    }
+    
+    func UnLock(){
+        IsLocked=false
     }
     
 }
